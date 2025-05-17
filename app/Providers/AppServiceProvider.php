7eb8120b595355499c8169df3e030baea6d28181
«jcano1» <javier.cano2004@gmail.com>
2025-05-17 19:59:25 +0200

    Fix missing URL import to force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // ✅ ESTA LÍNEA ES IMPORTANTE
 
 class AppServiceProvider extends ServiceProvider
 {
